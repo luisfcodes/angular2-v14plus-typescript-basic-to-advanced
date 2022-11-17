b4302d4505ef6e185265b307ca8f6703945dabf9
Luis Fernando <luis.silva9902@gmail.com>
2022-11-16 23:42:46 -0300

    Applying the OnInit and OnChange methods

--- my-first-app/src/app/title/title.component.ts
+++ my-first-app/src/app/title/title.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss']
 })
-export class TitleComponent implements OnInit {
-  public title = "Hello World"
+export class TitleComponent implements OnInit, OnChanges {
+  
+  @Input() public title = "Hello World"
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(): void {
+    console.log('Houve mudança.')
   }
 
 }
