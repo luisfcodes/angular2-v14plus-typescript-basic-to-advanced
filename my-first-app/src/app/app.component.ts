798ea97b5e0469dfd191d5a1a1f703c515d4cd55
Luis Fernando <luis.silva9902@gmail.com>
2022-11-25 08:46:37 -0300

    execution of ngModel

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -3,8 +3,8 @@ import {Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <app-data-binding></app-data-binding>
-    <app-structural-directives></app-structural-directives>
+    <!-- <app-data-binding></app-data-binding>
+    <app-structural-directives></app-structural-directives> -->
     <app-attributes-directives></app-attributes-directives>
     <router-outlet></router-outlet>
   `
--- my-first-app/src/app/attributes-directives/attributes-directives.component.ts
+++ my-first-app/src/app/attributes-directives/attributes-directives.component.ts
@@ -10,6 +10,9 @@ export class AttributesDirectivesComponent implements OnInit {
   public value = true
   public height = "20px"
 
+  public name = ''
+  public list: Array<{name: string}> = []
+
   constructor() { }
 
   ngOnInit(): void {
@@ -20,11 +23,16 @@ export class AttributesDirectivesComponent implements OnInit {
       }
 
       if(this.height === '20px'){
-        this.height = '50px'
+        this.height = '21px'
       } else {
         this.height = '20px'
       }
     }, 2000)
+  }
+
+  public add(){
+    this.list.push({name: this.name})
+    this.name = ''
   }
 
 }
