04cbafeff7e12540e721600414ca261300328eb5
Luis Fernando <luis.silva9902@gmail.com>
2022-11-28 22:55:32 -0300

    Execution of pipes

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -5,11 +5,13 @@ import {Component, OnInit } from '@angular/core';
   template: `
     <!-- <app-data-binding></app-data-binding>
     <app-structural-directives></app-structural-directives> -->
-    <app-attributes-directives>
+    <!-- <app-attributes-directives>
       <h1>Aulas de Diretivas Atributo</h1>
       <hr>
     </app-attributes-directives>
-    <router-outlet></router-outlet>
+    <router-outlet></router-outlet> -->
+
+    <app-attributes-directives></app-attributes-directives>
   `
 })
 export class AppComponent implements OnInit {
