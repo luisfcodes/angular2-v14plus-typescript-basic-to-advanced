d2087e373460a0b559df9566db5c6843784d0b70
Luis Fernando <luis.silva9902@gmail.com>
2022-12-02 15:31:42 -0300

    @Input execution

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,26 +1,35 @@
-import {Component, OnInit } from '@angular/core';
+import {Component, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
     <!-- <app-data-binding></app-data-binding>
-    <app-structural-directives></app-structural-directives> -->
-    <!-- <app-attributes-directives>
+    <app-structural-directives></app-structural-directives>
+    <app-attributes-directives>
       <h1>Aulas de Diretivas Atributo</h1>
       <hr>
     </app-attributes-directives>
-    <router-outlet></router-outlet> -->
+    <router-outlet></router-outlet>
+    <app-attributes-directives></app-attributes-directives> 
+    <app-new-component></app-new-component> -->
 
-    <app-new-component></app-new-component>
-    <app-attributes-directives></app-attributes-directives>
+    <app-input [count]="addValue"></app-input>
+    <button (click)="countAdd()">+1</button>
+    <router-outlet></router-outlet>
   `
 })
 export class AppComponent implements OnInit {
+
+  public addValue = 10
 
   constructor() {}
 
   ngOnInit(): void {
     
+  }
+
+  public countAdd(){
+    this.addValue += 1
   }
 
 }
--- my-first-app/src/app/shared/shared.module.ts
+++ my-first-app/src/app/shared/shared.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewComponentComponent } from './new-component/new-component.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [
     NewComponentComponent,
+    InputComponent,
   ],
   exports: [
-    NewComponentComponent
+    NewComponentComponent,
+    InputComponent
   ],
   imports: [
     CommonModule
